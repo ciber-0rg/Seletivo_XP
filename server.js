62ae07f2ef7f766bd98cac59014f37e9e66e7ef0
ciber_org <rudalemos.contato@gmail.com>
2022-07-24 21:18:16 -0300

    feat: HEROKU is ON!

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 const { response } = require('./server/app');
 const app = require('./server/app');
 
-const PORT = process.env.PORT || 3307;
+// const PORT = process.env.PORT || 3307;
 
-app.get('/', (_req, res) => res.send('Processo Seletivo XP!'));
+// app.get('/', (_req, res) => res.send('Processo Seletivo XP!'));
 
-app.listen(PORT, () => console.log('ouvindo porta', PORT));
+// app.listen(PORT, () => console.log('ouvindo porta', PORT));
 
-// app.set('port', (process.env.MYSQL_PORT || 3307));
-// app
-//     .get('/', function(_req, res) { res.send('Processo Seletivo XP!')})
-//     .listen(app.get('port'), function() {
-//         console.log('App is running, server is listening on port', app.get('port'));
-//     });
+app.set('port', (process.env.MYSQL_PORT || 3307));
+app
+    .get('/', function(_req, res) { res.send('Processo Seletivo XP!')})
+    .listen(app.get('port'), function() {
+        console.log('App is running, server is listening on port', app.get('port'));
+    });
