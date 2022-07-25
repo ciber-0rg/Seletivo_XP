b0b0151613010b246fd9805a73e7bd413081530d
ciber_org <rudalemos.contato@gmail.com>
2022-07-24 21:06:44 -0300

    fix: trying to fix deployment

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ require('dotenv').config();
 const { response } = require('./server/app');
 const app = require('./server/app');
 
-const port = process.env.MYSQL_PORT || 3307;
+const PORT = process.env.PORT || 3307;
 
 app.get('/', (_req, res) => res.send('Processo Seletivo XP!'));
 
-app.listen(port, () => console.log('ouvindo porta', port));
+app.listen(PORT, () => console.log('ouvindo porta', PORT));
+
+// app.set('port', (process.env.MYSQL_PORT || 3307));
+// app
+//     .get('/', function(_req, res) { res.send('Processo Seletivo XP!')})
+//     .listen(app.get('port'), function() {
+//         console.log('App is running, server is listening on port', app.get('port'));
+//     });
